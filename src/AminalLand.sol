87c54898ecf85a5e4a8da43e0b45e340f3082730
Will Papper <contact@papper.me>
2022-11-07 11:32:17 +0100

    Added todos and AminalLand stub

---
+++ src/AminalLand.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+// Add Land
+// Add plant slots for fertilizer
+// Add Harbinger tax and new land generation
+
+contract AminalLand {
+
+}
